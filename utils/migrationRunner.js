ee1d52f11b5fd539504f97c8f8498cd28e558503
Ajay <ajaymungur@hotmail.com>
2021-06-01 17:28:18 +0100

    Updated DB migration runner and reverted bcrypt version

--- utils/migrationRunner.js
+++ utils/migrationRunner.js
@@ -2,7 +2,7 @@ const {exec} = require('child_process');
 
 module.exports = {
   migrate: async () => {
-    const { stdout, stderr } = await exec('sequelize db:migrate');
+    const { stdout, stderr } = await exec('npx sequelize db:migrate');
     stdout ? console.log("Migration completed") : console.error('stderr:', stderr);
   }
 }
